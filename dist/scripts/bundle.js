98f0913ea9cd1b5c99e7c51379ea6e9e4efcda36
Hagan McPhail <hagan.lfd36@gmail.com>
2016-03-31 11:07:33 -0500

    added searchbox dropdown for desktop

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -4,6 +4,18 @@ $(document).ready(function(){
     $('.featured div').removeClass("active");
     $(this).addClass("active");
   });
+  $('.display-search').click(function(){
+    if ($(this).hasClass('active')){
+      $('.search-container').hide("slide", { direction: "right" }, 400);
+      $('.search-box').slideUp(400);
+      $(this).removeClass('active');
+    } else {
+      $('.search-box').slideDown(400);
+      $('.search-container').show("slide", { direction: "left" }, 800);
+      $(this).addClass('active');
+    }
+
+  });
 });
 
 },{}]},{},[1]);
--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
     $('.featured div').removeClass("active");
     $(this).addClass("active");
   });
+  $('.display-search').click(function(){
+    if ($(this).hasClass('active')){
+      $('.search-container').hide("slide", { direction: "right" }, 400);
+      $('.search-box').slideUp(400);
+      $(this).removeClass('active');
+    } else {
+      $('.search-box').slideDown(400);
+      $('.search-container').show("slide", { direction: "left" }, 800);
+      $(this).addClass('active');
+    }
+
+  });
 });
