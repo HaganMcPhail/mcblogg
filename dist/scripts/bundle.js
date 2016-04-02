c4319be9ca13f6f1ab2c832daf6d9cb8b6d174fe
Hagan McPhail <hagan.lfd36@gmail.com>
2016-04-02 02:45:13 -0500

    added some archive functionality; working on converting application to a react app

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -1,29 +1,75 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 $(document).ready(function(){
 
-  // featured content selector
+  var removeActiveLinks = function (){
+    $('a[class^="display-"]').removeClass('active');
+  }
+
+  var removeActiveSection = function (activeSection){
+    $(activeSection).removeClass('active');
+  }
+
+  var hideActiveSection = function (item, activeSection){
+    if (activeSection.length > 0) {
+      $(activeSection).hide("slide", { direction: "right" }, 300, function(){
+        $('.' + item + '-box').show("slide", { direction: "up" }, 300);
+      });
+    } else {
+      $('.' + item + '-box').show("slide", { direction: "up" }, 300);
+    }
+  }
+
+  var loadPosts =  function() {
+    $(".blog-preview").hide();
+    $(".blog-preview").each(function(index) {
+      $(this).delay(100*index).fadeIn(300);
+    });
+  }
+
+  // menu bar interaction
+  $('a[class^="display-"]').on("click", function(){
+    var item = $(this).attr('item');
+    if ($(this).hasClass('active')){
+      $('.' + item + '-box').hide("slide", { direction: "right" }, 300);
+      $(this).removeClass('active');
+      $('body').css('overflow', 'visible');
+    } else {
+      var activeSection = $('section.active');
+      removeActiveLinks();
+      hideActiveSection(item, activeSection);
+      removeActiveSection(activeSection);
+      $(this).addClass('active');
+      $('.' + item + '-box').addClass('active');
+      $('body').css('overflow', 'hidden');
+    }
+  });
+
+  $('.years a').click(function(){
+    $('.months').show();
+    $('.years').hide();
+  });
+
+  $('.months a').click(function(){
+    removeActiveLinks();
+    $('.archive-box').hide("slide", { direction: "right" }, 300, function(){
+      $('.months').hide();
+      $('.years').show();
+      $('body').css('overflow', 'visible');
+    });
+    removeActiveSection($('section.active'));
+    $('.featured-toggle').hide();
+    $('.archive-header').show();
+    loadPosts();
+  });
+
   $('.featured div').click(function(){
     $('.featured div').removeClass("active");
     $(this).addClass("active");
+    loadPosts();
   });
 
-  // search bar
-  $('a[class^="display-"]').click(function(){
-    var item = $(this).attr('item');
-    if ($(this).hasClass('active')){
-      $('.' + item + '-box').hide("slide", { direction: "up" }, 300);
-      $(this).removeClass('active');
-    } else {
-      $('a[class^="display-"]').removeClass('active');
-      $('section[class$="-box"]').hide("slide", { direction: "up" }, 300, function(){
-        setTimeout(function(){
-          $('.' + item + '-box').show("slide", { direction: "up" }, 300);
-        }, 300);
-      });
-      $(this).addClass('active');
-    }
-
-  });
+  loadPosts();
+  $('footer').show();
 });
 
 },{}]},{},[1]);
--- src/main.js
+++ src/main.js
@@ -1,26 +1,72 @@
 $(document).ready(function(){
 
-  // featured content selector
+  var removeActiveLinks = function (){
+    $('a[class^="display-"]').removeClass('active');
+  }
+
+  var removeActiveSection = function (activeSection){
+    $(activeSection).removeClass('active');
+  }
+
+  var hideActiveSection = function (item, activeSection){
+    if (activeSection.length > 0) {
+      $(activeSection).hide("slide", { direction: "right" }, 300, function(){
+        $('.' + item + '-box').show("slide", { direction: "up" }, 300);
+      });
+    } else {
+      $('.' + item + '-box').show("slide", { direction: "up" }, 300);
+    }
+  }
+
+  var loadPosts =  function() {
+    $(".blog-preview").hide();
+    $(".blog-preview").each(function(index) {
+      $(this).delay(100*index).fadeIn(300);
+    });
+  }
+
+  // menu bar interaction
+  $('a[class^="display-"]').on("click", function(){
+    var item = $(this).attr('item');
+    if ($(this).hasClass('active')){
+      $('.' + item + '-box').hide("slide", { direction: "right" }, 300);
+      $(this).removeClass('active');
+      $('body').css('overflow', 'visible');
+    } else {
+      var activeSection = $('section.active');
+      removeActiveLinks();
+      hideActiveSection(item, activeSection);
+      removeActiveSection(activeSection);
+      $(this).addClass('active');
+      $('.' + item + '-box').addClass('active');
+      $('body').css('overflow', 'hidden');
+    }
+  });
+
+  $('.years a').click(function(){
+    $('.months').show();
+    $('.years').hide();
+  });
+
+  $('.months a').click(function(){
+    removeActiveLinks();
+    $('.archive-box').hide("slide", { direction: "right" }, 300, function(){
+      $('.months').hide();
+      $('.years').show();
+      $('body').css('overflow', 'visible');
+    });
+    removeActiveSection($('section.active'));
+    $('.featured-toggle').hide();
+    $('.archive-header').show();
+    loadPosts();
+  });
+
   $('.featured div').click(function(){
     $('.featured div').removeClass("active");
     $(this).addClass("active");
+    loadPosts();
   });
 
-  // search bar
-  $('a[class^="display-"]').click(function(){
-    var item = $(this).attr('item');
-    if ($(this).hasClass('active')){
-      $('.' + item + '-box').hide("slide", { direction: "up" }, 300);
-      $(this).removeClass('active');
-    } else {
-      $('a[class^="display-"]').removeClass('active');
-      $('section[class$="-box"]').hide("slide", { direction: "up" }, 300, function(){
-        setTimeout(function(){
-          $('.' + item + '-box').show("slide", { direction: "up" }, 300);
-        }, 300);
-      });
-      $(this).addClass('active');
-    }
-
-  });
+  loadPosts();
+  $('footer').show();
 });
